2ea3e9d2ce72a87a15a23d4ea83fbb44414b82c0
Bert Hajee <hajee@moretIA.com>
2015-03-29 21:39:36 +0200

    Added valid scope to the values command

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,8 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "hajee/centos-5.10-x86_64"
 
+  config.vm.hostname = "test.development.org"
+
   config.vm.provision :shell, :inline => "rm -rf /etc/puppet/modules/connect; ln -s /vagrant /etc/puppet/modules/connect"
   config.vm.provision :shell, :inline => "ln -sf /vagrant/hiera.yaml /etc/puppet/hiera.yaml"
   config.vm.provision :shell, :inline => "ln -sf /vagrant/examples /etc/puppet/connect"
--- lib/puppet/face/connect/values.rb
+++ lib/puppet/face/connect/values.rb
@@ -13,7 +13,6 @@ Puppet::Face.define(:connect, '0.0.1') do
 
   action(:values) do
    default
-
     summary "List the value(s) specfied in the connect config file(s)"
 
     description <<-EOT
@@ -44,7 +43,7 @@ Puppet::Face.define(:connect, '0.0.1') do
       config_dir = Puppet['confdir']
       Hiera.new(:config => "#{config_dir}/hiera.yaml")
       backend = Hiera::Backend::Connect_backend.new
-      values_list = backend.lookup_values(name, {}, false, 1)
+      values_list = backend.lookup_values(name, scope, false, 1)
       output = ''
       values_list.each do | parameter, value|
         output << "#{parameter} = #{value.ai(:indent => 2)}\n"
@@ -52,7 +51,37 @@ Puppet::Face.define(:connect, '0.0.1') do
       output
     end
 
+
   end
+
+  #
+  # This piece of copde is borrowed from Puppet. It create'a a valid Puppet scope to pass
+  # to the hier backend. The scope only contains the facter values. 
+  #
+  def scope
+    nodename = Facter.value('hostname')
+    fact_values = Facter.to_hash
+    node = Puppet::Node.new("testnode", :facts => Puppet::Node::Facts.new("facts", fact_values))
+    compiler = Puppet::Parser::Compiler.new(node)
+    # configure compiler with facts and node related data
+    # Set all global variables from facts
+    fact_values.each {|param, value| compiler.topscope[param] = value }
+    # Configured trusted data (even if there are none)
+    compiler.topscope.set_trusted(node.trusted_data)
+    # Set the facts hash
+    # compiler.topscope.set_facts(fact_values)
+
+    # pretend that the main class (named '') has been evaluated
+    # since it is otherwise not possible to resolve top scope variables
+    # using '::' when rendering. (There is no harm doing this for the other actions)
+    #
+    compiler.topscope.class_set('', compiler.topscope)
+    scope = Puppet::Parser::Scope.new(compiler)
+    scope.source = Puppet::Resource::Type.new(:node, nodename)
+    scope.parent = compiler.topscope
+    scope
+  end
+
 
 end
 
