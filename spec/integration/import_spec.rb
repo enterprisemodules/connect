90ade78dd5569842c08b7c0fcaa7ceec5915cebd
Bernard Pietraga <bernard.pietraga@gmail.com>
2018-03-19 18:01:29 +0100

    [test] Refactor spec

--- spec/integration/import_spec.rb
+++ spec/integration/import_spec.rb
@@ -2,13 +2,10 @@ require 'spec_helper'
 require 'connect/dsl'
 
 RSpec.describe 'dsl import' do
-
   let(:dsl) { Connect::Dsl.new}
 
   context 'importing' do
-
     context 'using YAML importer' do
-
       context 'without scope' do
         it 'is retrievable' do
           dsl.parse(<<-EOD)
@@ -16,7 +13,7 @@ RSpec.describe 'dsl import' do
             a = 'existing_entry'
           }
           EOD
-          expect(dsl.lookup_value("a")).to eql('this is succesfully returned')
+          expect(dsl.lookup_value("a")).to eql('this is successfully returned')
         end
       end
 
@@ -27,14 +24,9 @@ RSpec.describe 'dsl import' do
             a = 'existing_entry'
           }
           EOD
-          expect(dsl.lookup_value("imported::a")).to eql('this is succesfully returned')
+          expect(dsl.lookup_value("imported::a")).to eql('this is successfully returned')
         end
       end
-
-
     end
-
   end
-
 end
-
